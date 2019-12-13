edf846be9003bce448bb3709fd44624b1b1f69d4
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-13 13:07:48 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :bills
+
+  resources :clients
+
+  resources :companies
+
+  resources :items
+
+  resources :bank_accounts
+
+  get "/dashboard", to: "pages#dashboard"
 end
