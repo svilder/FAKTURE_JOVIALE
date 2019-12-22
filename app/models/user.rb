6f24a1c6360277e2701f840e1921850d4c82b6ca
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-22 15:24:54 +0100

    user clients has many machin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :clients
+  has_many :clients through: :bills
   has_many :bills
   has_many :bank_accounts
 end
