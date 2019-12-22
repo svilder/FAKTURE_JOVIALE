ff1734b4c4a4b4ef12b1fb10c1a956ed651181e7
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-22 14:23:08 +0100

    seed pour console

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ User.destroy_all
 Bill.destroy_all
 Client.destroy_all
 Companie.destroy_all
-Company.destroy_all
 Item.destroy_all
 BankAccount.destroy_all
 
-user = User.new(email:"yo@yo.com", password:"yoyoyo",)
-bill = Bill.new(title:"Facture Wagon automne", validated:false, paid:false, sent:false, user_id:1, client_id:1)
+user = User.new(email:"yo@yo.com", password:"yoyoyo")
 companie = Companie.new(name:"Ubisoft", city:"Montreal")
+client = Client.new(first_name:"Bob", last_name:"Poop", email:"bob@poop.com", companie_id:1)
+bill = Bill.new(title:"Facture Wagon automne", validated:false, paid:false, sent:false, user_id:1, client_id:1)
